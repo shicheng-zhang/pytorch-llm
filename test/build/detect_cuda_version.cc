fdc426534ee1c25474fd98f8f6c26dd3b056db35
shicheng-zhang <shichenghome@gmail.com>
2026-04-25 09:19:32 -0600

    25/04/26: Some Formatting Shennanigans

--- test/build/detect_cuda_version.cc
+++ test/build/detect_cuda_version.cc
@@ -1,6 +1,6 @@
 #include <cuda.h>
-#include <cstdio>
-int main() {
-  printf("%d.%d", CUDA_VERSION / 1000, (CUDA_VERSION / 10) % 100);
-  return 0;
+#include <stdio.h>
+int main () {
+    printf ("%d.%d", CUDA_VERSION / 1000, (CUDA_VERSION / 10) % 100);
+    return 0;
 }
